f1b7d4a91acf4f348591ca7bffb1a866d6032ce9
MohamedAslam971 <mohamedaslam0797@gmail.com>
2025-03-08 21:50:30 +0400

    tailwind ready

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ function App() {
 
   return (
     <>
-      <h1>hello world</h1>
+      <h1 className="text-red-500">hello world</h1>
+      <h1 className="text-blue-600">hello world</h1>
     </>
   )
 }
